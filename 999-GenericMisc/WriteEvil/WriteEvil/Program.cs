2dc813d83d53e70f9448ffcf796e8f5ed5539c3d
n0isegat3 <jan@cyber-rangers.com>
2022-05-04 13:09:01 +0200

    write console line samples

---
+++ 999-GenericMisc/WriteEvil/WriteEvil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WriteEvil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Evil from n0isegat3!");
+        }
+    }
+}
---
+++ 999-GenericMisc/WriteHelloWorld/WriteHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WriteHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from n0isegat3!");
+        }
+    }
+}
